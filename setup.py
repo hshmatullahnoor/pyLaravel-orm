754ff84fa72592e40ee49454b88a4e66d9a2c978
Hshamt Ullah <hshmatullahnoor@gmail.com>
2025-08-26 11:22:31 +0430

    Bump version to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pylaravel-orm",
-    version="0.1.0",
+    version="0.1.1",
     author="Meysam Afghan",
     author_email="meysamnoori010@gmail.com",
     description="A Laravel-like ORM for MySQL in Python",
